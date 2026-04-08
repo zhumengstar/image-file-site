c7884868ceb38bd75872d3183136f626e20a95fa
zhumengstar <1378484330@qq.com>
2026-04-08 17:55:56 +0800

    feat: add file management support with navigation, upload handling, and directory exclusion patterns

--- server_flask.py
+++ server_flask.py
@@ -29,35 +29,68 @@ def static_files(path):
 def get_images():
     images = []
     for filename in os.listdir(UPLOAD_DIR):
-        if os.path.isfile(os.path.join(UPLOAD_DIR, filename)):
+        if os.path.isfile(os.path.join(UPLOAD_DIR, filename)) and filename.startswith('image_'):
             images.append({
                 'name': filename,
                 'url': f'http://localhost:8000/{UPLOAD_DIR}/{filename}'
             })
     return jsonify(images)
 
-# 上传图片
+# 获取文件列表
+@app.route('/files')
+def get_files():
+    files = []
+    for filename in os.listdir(UPLOAD_DIR):
+        if os.path.isfile(os.path.join(UPLOAD_DIR, filename)) and filename.startswith('file_'):
+            file_size = os.path.getsize(os.path.join(UPLOAD_DIR, filename))
+            files.append({
+                'name': filename,
+                'url': f'http://localhost:8000/{UPLOAD_DIR}/{filename}',
+                'size': file_size
+            })
+    return jsonify(files)
+
+# 上传文件
 @app.route('/upload', methods=['POST'])
-def upload_image():
-    if 'image' not in request.files:
+def upload_file():
+    if 'image' in request.files:
+        file = request.files['image']
+        if file.filename == '':
+            return jsonify({'error': 'No file selected'}), 400
+        
+        # 生成唯一文件名
+        unique_id = str(uuid.uuid4())
+        name, ext = os.path.splitext(file.filename)
+        new_filename = f'image_{unique_id}{ext}'
+        filepath = os.path.join(UPLOAD_DIR, new_filename)
+        
+        file.save(filepath)
+        
+        return jsonify({
+            'url': f'http://localhost:8000/{UPLOAD_DIR}/{new_filename}',
+            'name': new_filename
+        })
+    elif 'file' in request.files:
+        file = request.files['file']
+        if file.filename == '':
+            return jsonify({'error': 'No file selected'}), 400
+        
+        # 生成唯一文件名
+        unique_id = str(uuid.uuid4())
+        name, ext = os.path.splitext(file.filename)
+        new_filename = f'file_{unique_id}{ext}'
+        filepath = os.path.join(UPLOAD_DIR, new_filename)
+        
+        file.save(filepath)
+        file_size = os.path.getsize(filepath)
+        
+        return jsonify({
+            'url': f'http://localhost:8000/{UPLOAD_DIR}/{new_filename}',
+            'name': new_filename,
+            'size': file_size
+        })
+    else:
         return jsonify({'error': 'No file uploaded'}), 400
-    
-    file = request.files['image']
-    if file.filename == '':
-        return jsonify({'error': 'No file selected'}), 400
-    
-    # 生成唯一文件名
-    unique_id = str(uuid.uuid4())
-    name, ext = os.path.splitext(file.filename)
-    new_filename = f'image_{unique_id}{ext}'
-    filepath = os.path.join(UPLOAD_DIR, new_filename)
-    
-    file.save(filepath)
-    
-    return jsonify({
-        'url': f'http://localhost:8000/{UPLOAD_DIR}/{new_filename}',
-        'name': new_filename
-    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
